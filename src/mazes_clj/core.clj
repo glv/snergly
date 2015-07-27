7258f548cddab3539384e639233a0bcc61945217
Glenn Vanderburg <glv@vanderburg.org>
2015-07-26 23:46:38 -0500

    Basic grid and cell functions. plus binary tree algorithm.

--- src/mazes_clj/core.clj
+++ src/mazes_clj/core.clj
@@ -1,6 +1,86 @@
 (ns mazes-clj.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-cell
+  [row column rows columns]
+  {:type  :Cell
+   :coord [row column]
+   :north (when (> row 0) [(dec row) column])
+   :south (when (< row (dec rows)) [(inc row) column])
+   :west  (when (> column 0) [row (dec column)])
+   :links #{}})
+
+(defn make-grid
+  "Creates and returns a new grid with the specified row and column sizes."
+  [rows columns]
+  {:type    :Grid
+   :rows    rows
+   :columns columns
+   :cells   (into [] (for [row (range rows) column (range columns)]
+                       (make-cell row column rows columns)))})
+
+(defn cell-index
+  ([grid [row column]] (cell-index grid row column))
+  ([grid row column] (+ (* row (:columns grid)) column)))
+
+(defn grid-cell
+  ([grid [row column]] (grid-cell grid row column))
+  ([grid row column]
+   ((:cells grid) (cell-index grid row column))))
+
+(defn random-cell [{rows :rows columns :columns :as grid}]
+  (let [row (rand-int rows)
+        column (rand-int columns)]
+    (grid-cell grid row column)))
+
+(defn grid-size [{rows :rows columns :columns}]
+  (* rows columns))
+
+(defn grid-rows [{cells :cells columns :columns}]
+  (partition columns cells))
+
+(defn grid-cells [grid]            ; just for a name to parallel grid-rows
+  (:cells grid))
+
+(defn link-cells [{cells :cells :as grid}
+                  {cell-coord :coord cell-links :links :as cell} neighbor-coord]
+  (let [neighbor (grid-cell grid neighbor-coord)
+        neighbor-links (:links neighbor)]
+    (assoc grid :cells
+                (assoc cells (cell-index grid cell-coord)
+                             (assoc cell :links (conj cell-links neighbor-coord))
+                             (cell-index grid neighbor-coord)
+                             (assoc neighbor :links (conj neighbor-links cell-coord))))))
+
+(defn linked? [cell other-cell-coord]
+  (contains? (:links cell) other-cell-coord))
+
+(defn print-grid [{rows :rows columns :columns :as grid}]
+  (println (apply str "+" (repeat columns "---+")))
+  (doseq [row (grid-rows grid)]
+    ;; cell space line
+    (println (apply str "|"
+                    (for [cell row]
+                      (str "   " (if (linked? cell (:east cell))
+                                   " "
+                                   "|")))))
+    ;; southern separator line
+    (println (apply str "+"
+                    (for [cell row]
+                      (str (if (linked? cell (:south cell))
+                             "   "
+                             "---") "+"))))))
+
+
+
+(defn maze-binary-tree [grid]
+  (loop [g grid
+         [cell & cells] (:cells grid)]
+    (let [neighbors (filter identity (map cell [:north :east]))
+          neighbor (when-not (empty? neighbors)
+                     (rand-nth neighbors))
+          new-grid (if (nil? neighbor)
+                     g
+                     (link-cells g cell neighbor))]
+      (if (empty? cells)
+        new-grid
+        (recur new-grid cells)))))
--- test/mazes_clj/core_test.clj
+++ test/mazes_clj/core_test.clj
@@ -2,6 +2,86 @@
   (:require [clojure.test :refer :all]
             [mazes-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest t-make-cell
+  (testing "middle cell"
+    (let [cell (make-cell 3 5 10 15)]
+      (are [expected key] (= expected (key cell))
+                        :Cell :type
+                        [3 5] :coord
+                        [2 5] :north
+                        [4 5] :south
+                        [3 6] :east
+                        [3 4] :west
+                        #{}   :links)))
+  (testing "north border cell"
+    (let [cell (make-cell 0 5 10 15)]
+      (are [expected key] (= expected (key cell))
+                          [0 5] :coord
+                          nil   :north
+                          [1 5] :south
+                          [0 6] :east
+                          [0 4] :west)))
+  (testing "south border cell"
+    (let [cell (make-cell 9 5 10 15)]
+      (is (= [9 5] (:coord cell)))
+      (is (nil? (:south cell)))))
+  (testing "east border cell"
+    (let [cell (make-cell 3 14 10 15)]
+      (is (= [3 14] (:coord cell)))
+      (is (nil? (:east cell)))))
+  (testing "west border cell"
+    (let [cell (make-cell 3 0 10 15)]
+      (is (= [3 0] (:coord cell)))
+      (is (nil? (:west cell))))))
+
+(deftest t-make-grid
+  (let [grid (make-grid 2 3)]
+    (are [expected key] (= expected (key grid))
+                        :Grid :type
+                        2 :rows
+                        3 :columns)
+    (is (= 6 (count (:cells grid))))
+    (is (= (make-cell 1 1 2 3)
+           ((:cells grid) 4)))
+    (is (= (make-cell 0 2 2 3)
+           ((:cells grid) 2)))))
+
+(deftest t-cell-index
+  (let [grid3x5 (make-grid 3 5)
+        grid5x3 (make-grid 5 3)]
+    (is (= 6 (cell-index grid3x5 1 1)))
+    (is (= 4 (cell-index grid5x3 1 1)))
+    (is (= 14 (cell-index grid3x5 2 4)))
+    (is (= 14 (cell-index grid5x3 4 2)))))
+
+(deftest t-grid-cell
+  (let [grid3x5 (make-grid 3 5)]
+    (is (= (make-cell 2 3 3 5)
+           (grid-cell grid3x5 2 3)))
+    (is (= (make-cell 2 3 3 5)
+           (grid-cell grid3x5 [2 3])))))
+
+(deftest t-grid-rows
+  (let [grid3x5 (make-grid 3 5)
+        rowseq (grid-rows grid3x5)]
+    (is (seq? rowseq))
+    (is (= 3 (count rowseq)))
+    (doseq [row rowseq]
+      (is (seq? row))
+      (is (= 5 (count row))))
+    (is (= (make-cell 1 3 3 5)
+           (-> rowseq (nth 1) (nth 3))))
+    (is (= (make-cell 2 4 3 5)
+           (-> rowseq (nth 2) (nth 4))))))
+
+(deftest t-link-cells
+  (let [grid3x5 (make-grid 3 5)]
+    (let [linked-grid (link-cells grid3x5
+                                  (grid-cell grid3x5 1 3)
+                                  [2 3])]
+      (is (contains? (:links (grid-cell linked-grid 1 3)) [2 3]))
+      (is (contains? (:links (grid-cell linked-grid 2 3)) [1 3]))
+      )))
+
+(deftest t-maze-binary-tree
+  (print-grid (maze-binary-tree (make-grid 5 5))))
