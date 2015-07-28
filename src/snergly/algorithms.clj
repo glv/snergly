4857c7fad4315d581ca308483a8c0a18952b7f2d
Glenn Vanderburg <glv@vanderburg.org>
2015-07-28 15:32:28 -0500

    Further decomposition of sidewinder.
    The logic for deciding to end the current run of cells is inherently
    a bit complex.  The way Jamis' Ruby version is coded seemed clear,
    but when I coded it that way in Clojure it felt annoyingly complex
    and like it was working against the rest of the code (in the sense
    that the intermediate values I prepared for doing that test were the
    opposite of what was needed for taking the next steps).  I wondered
    if doing a De Morgan's Law inversion on it would feel better.
    
    The logic was complex enough that I wrote a test to verify that the
    inverted version was equivalent, and then rewrote `sidewinder-step`
    to use the inverted version.  It *did* feel cleaner, and that helped
    me to find ways to decompose the function into smaller pieces.
    
    But then a funny thing happened: once the code was decomposed, the
    test for ending the run was nicely isolated, and it no longer mattered
    that it didn't mesh well with the subsequent steps.  And the original
    version seems a little easier to understand in isolation, not to mention
    that there's value in having it align well with the Ruby code in the
    book.  So I switched it back.
    
    This commit leaves both versions in (along with the test I used to
    verify the correctess of the inverted version) just so they'll be in
    the repo history.  The next commit will pull those out.

--- src/snergly/algorithms.clj
+++ src/snergly/algorithms.clj
@@ -1,6 +1,19 @@
 (ns snergly.algorithms
   (:require [snergly.grid :refer :all]))
 
+;; Naming conventions:
+;;
+;; Top-level maze algorithm functions in this namespace are
+;; named "maze-name", where "name" is the name of the algorithm
+;; (for example, "maze-sidewinder"). Anytime a new algorithm is
+;; added, the name should also be added to snergly.core/algorithms
+;; (at least until I decide whether that vector should be build
+;; automatically from this namespace).
+;;
+;; Helper functions used for only a particular algorithm should
+;; have the algorithm name as a name prefix (for example,
+;; sidewinder-step).
+
 (defn binary-tree-step [grid coord]
   (let [cell (grid-cell grid coord)
         neighbors (filter identity (map cell [:north :east]))]
@@ -11,21 +24,34 @@
 (defn maze-binary-tree [grid]
   (reduce binary-tree-step grid (grid-coords grid)))
 
+(defn sidewinder-end-run? [cell]
+  (let [on-east-side? (not (:east cell))
+        on-north-side? (not (:north cell))]
+    (or on-east-side?
+        (and (not on-north-side?)
+             (= 0 (rand-int 2))))))
+
+(defn sidewinder-continue-run? [cell]
+  (let [east-neighbor (:east cell)
+        north-neighbor (:north cell)]
+    (and east-neighbor
+         (not (and north-neighbor
+                   (= 0 (rand-int 2)))))))
+
+(defn sidewinder-end-run [grid run]
+  (let [cell (grid-cell grid (rand-nth run))
+        north-neighbor (:north cell)]
+    (if north-neighbor
+      (link-cells grid cell north-neighbor)
+      grid)))
+
 (defn sidewinder-step [grid coord run]
-  (let [resolve (partial grid-cell grid)
-        cell (resolve coord)
-        east-side? (nil? (:east cell))
-        north-side? (nil? (:north cell))
-        finish-run? (or east-side?
-                        (and (not north-side?)
-                             (= 0 (rand-int 2))))
-        new-grid (if finish-run?
-                   (let [member (resolve (rand-nth run))]
-                     (if (:north member)
-                       (link-cells grid member (:north member))
-                       grid))
+  (let [cell (grid-cell grid coord)
+        end-run? (sidewinder-end-run? cell)
+        new-grid (if end-run?
+                   (sidewinder-end-run grid run)
                    (link-cells grid cell (:east cell)))]
-    [new-grid (if finish-run? [] run)]))
+    [new-grid (if end-run? [] run)]))
 
 (defn maze-sidewinder [grid]
   (loop [grid grid
