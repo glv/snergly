12bf26aea6113130b26e5d16157b0d41d7b85979
Glenn Vanderburg <glv@vanderburg.org>
2015-08-02 14:15:46 -0500

    Basic figwheel setup (straight from the template)

--- project.clj
+++ project.clj
@@ -5,4 +5,56 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot snergly.core
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.cli "0.3.1"]])
+                 [org.clojure/clojurescript "0.0-3297"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/tools.cli "0.3.1"]]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-figwheel "0.3.5"]]
+  :source-paths ["src"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+
+                        :figwheel {:on-jsload "snergly.core/on-js-reload"}
+
+                        :compiler {:main snergly.core
+                                   :asset-path "js/compiled/out"
+                                   :output-to "resources/public/js/compiled/snergly.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "min"
+                        :source-paths ["src"]
+                        :compiler {:output-to "resources/public/js/compiled/snergly.js"
+                                   :main snergly.core
+                                   :optimizations :advanced
+                                   :pretty-print false}}]}
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+
+             ;; Start an nREPL server into the running figwheel process
+             ;; :nrepl-port 7888
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server, this is for simple ring servers, if this
+             ;; doesn't work for you just run your own server :)
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+             })
