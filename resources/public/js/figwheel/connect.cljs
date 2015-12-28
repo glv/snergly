da122b6e1cb880cb1fed6948f9a5e47b738fc5ac
Glenn Vanderburg <glv@vanderburg.org>
2015-12-28 17:04:27 -0600

    Remove compiled js files

--- resources/public/js/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [snergly.core]))
-(figwheel.client/start {:build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
